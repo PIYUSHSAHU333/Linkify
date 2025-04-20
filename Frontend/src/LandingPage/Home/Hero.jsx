ca4942493a50a15dd098e05a0cbb9356b99bb62f
PiyushSahu <piyushsahuys@gmail.com>
2025-04-20 23:38:19 +0530

    corrections in hero of landing page

--- Frontend/src/LandingPage/Home/Hero.jsx
+++ Frontend/src/LandingPage/Home/Hero.jsx
@@ -4,7 +4,7 @@ import JoinCall from "./JoinCall";
 function Hero() {
   return (
     <>
-      <div className="hero  flex flex-col h-[700px] ">
+      <div className="hero  flex flex-col h-[700px] mb-20 ">
         <div className="w-full flex justify-between items-center">
           <img src="media/images/logo2.png" alt="#" style={{ width: "20%" }} />
 
@@ -18,20 +18,29 @@ function Hero() {
           </div>
         </div>
 
-        <div className="main-content flex-1 items-center flex justify-evenly w-full ">
+        <div className="main-content flex-col gap-4 flex-1 items-center flex w-full ">
+
+          <div className="div mb-16 flex justify-center items-center">
           <div class="notification">
             <div class="notiglow"></div>
             <div class="notiborderglow"></div>
             <div class="notititle">Welcome To VibeCall</div>
             <div class="notibody">please sign up or login if already a user</div>
           </div>
+          </div>
+          
+          
 
-          <div className="joincall ml-48">
+         
+          <div className="div2 flex justify-evenly gap-2 w-full mt-8">
+          <div className="joincall self-center ml-48 ">
             <JoinCall />
           </div>
           <div className="image mr-56">
             <img src="media/images/joincall.png" alt="" />
           </div>
+          </div>
+          
         </div>
       </div>
     </>
