205e7fe932bcb15c30353dc3b4c3db486f4c8ce9
PiyushSahu <piyushsahuys@gmail.com>
2025-04-21 16:17:03 +0530

    socket.io server side integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.set("port", process.env.PORT || 8080);
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
-app.use("/api/v1/users",userRoutes);
+app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
   const connectionDb = await mongoose.connect(process.env.MongoUrl);
